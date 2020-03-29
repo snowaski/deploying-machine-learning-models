e4484d1e66068b9fe25efcf2b2e40bd22b385972
Sydney Nowaski <snowaski32@gmail.com>
2020-03-28 21:04:13 -0500

    merge conflicts

--- packages/regression_model/regression_model/__init__.py
+++ packages/regression_model/regression_model/__init__.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import logging
 import os
 
@@ -11,11 +10,6 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 logger.addHandler(logging_config.get_console_handler())
 logger.propagate = False
-=======
-import os
-
-from regression_model.config import config
->>>>>>> 6162c318b58b225e0061fccd6c64cd67fe205c1b
 
 
 with open(os.path.join(config.PACKAGE_ROOT, 'VERSION')) as version_file:
